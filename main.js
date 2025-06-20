43baf4ccc90b9670cf4366acc07067bdce2b09e1
nicolashiguera17 <189983438+nicolashiguera17@users.noreply.github.com>
2025-06-20 14:07:12 -0500

    feat: :sparkles: configuracion final

---
+++ main.js
@@ -0,0 +1,17 @@
+function updateClocks() {
+    const clocks = document.querySelectorAll(".clock");
+    clocks.forEach(clock => {
+      const tz = clock.getAttribute("data-timezone");
+      const time = new Date().toLocaleTimeString("en-US", {
+        timeZone: tz,
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false
+      });
+      clock.textContent = time;
+    });
+  }
+  
+  setInterval(updateClocks, 1000);
+  updateClocks();
